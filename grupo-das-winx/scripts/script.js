1d7ccbacd8f859cb16b6b4b976c33369c912c626
Emanuelly_Brito <manu.gibson14@gmail.com>
2026-07-01 20:18:58 -0300

    arquivo scripts.script.js modificado

--- grupo-das-winx/scripts/script.js
+++ grupo-das-winx/scripts/script.js
@@ -2,3 +2,6 @@ function mostrarMensagem() {
     document.getElementById("mensagem").innerHTML =
          "Parebéns! Seu projeto está funcionando corretamente.";
 }
+window.onload = function () {
+    alert("Bem-vindo ao Site Full Stack Noite!")
+};
